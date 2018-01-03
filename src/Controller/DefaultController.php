ce81dd8cd9625ef7d5b506c89b8f86de877708ac
jjuarezvdc <jjuarezvdc@localhost>
2018-01-02 21:17:12 -0600

    llegue a menu

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,6 +15,7 @@ class DefaultController extends Controller{
                 </body>
                 <footer>
                     tex(esto solo esta en foobaz)to
+                    llegue a menu... voy a ponerlo
                 </footer>
             </html>
         ');

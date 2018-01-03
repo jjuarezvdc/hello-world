fb0e26bcf7bbfdcd227a09254e904eb6f9e0bb29
jjuarezvdc <jjuarezvdc@localhost>
2018-01-02 18:10:32 -0600

    initial checkin

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller{ 
+    public function index(){
+        return new Response('
+            <html>
+                <body>
+                    <h1>Hello Symfony 4 World - jj </h1>
+                </body>
+            </html>
+        ');
+    }
+}
